7c59e17a84238eb874dd3e4f1ce10ebe06eebafe
Jefferson Alves <jefff9910@outlook.com>
2021-02-04 22:14:42 -0300

    novas funcionalidades

--- scripts/script.js
+++ scripts/script.js
@@ -26,4 +26,19 @@ function start() {
 
 window.addEventListener("load", start);
 
+let bannerMain = document.getElementById('banner-content-1');
+let bannerMaintext = "Os melhores lugares do Brasil";
+let tam = bannerMaintext.length;
+let count = 0;
+
+
+setInterval(()=> {
+    count++;
+    let myText = bannerMaintext.substring(0, count);
+    bannerMain.innerText = myText + "|";
+    
+    if (count == tam) {
+        count = 0;
+    }
+}, 150)
 
